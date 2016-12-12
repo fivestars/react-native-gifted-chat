a79b3a5bfbb3cbbeefa44ef5aa42494afd359cbf
Kai Si <kai.si@fivestars.com>
2016-12-12 12:20:22 -0800

    Added paddingBottom and reverted lineHeight

--- src/MessageText.js
+++ src/MessageText.js
@@ -68,11 +68,12 @@ export default class MessageText extends React.Component {
 
 const textStyle = {
   fontSize: 16,
-  lineHeight: 24,
+  lineHeight: 20,
   marginTop: 5,
   marginBottom: 5,
   marginLeft: 10,
   marginRight: 10,
+  paddingBottom: 3,
 };
 
 const styles = {
